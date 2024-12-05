a60ad3f38125662b32e6d64d4ececac856f657d3
dedarob <euclidesroberto16@gmail.com>
2024-12-04 23:21:58 -0300

    adicionado axios e react-form-hooks para facilitar escalabilidade do código, implementado somente na tela de login TODO: search bar ainda não está pronta, falta decidir qual sua lógica

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,69 +1,43 @@
-import { useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
 import Footer from "../../components/Footer";
 import Container from "../../components/Container";
 import styles from "./Login.module.css";
 import { FaRecycle } from "react-icons/fa";
+import { useForm } from "react-hook-form";
 import { loginFetch } from "./loginService";
-
-function LoginHandler({ username, password }) {
-  const navigate = useNavigate();
-
-  const handleLogin = () => {
-    loginFetch(username, password)
-      .then((data) => {
-        if (data.token) {
-          localStorage.setItem("authToken", data.token);
-          console.log("Token:", data.token);
-          navigate("/home");
-        } else {
-          console.log("Login failed");
-        }
-      })
-      .catch((error) => {
-        console.log("Error while attempting login:", error);
-      });
-  };
-
-  return (
-    <button className={styles.login_button} onClick={handleLogin}>
-      Login
-    </button>
-  );
-}
+import { useNavigate } from "react-router-dom";
 
 function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    loginFetch(data, navigate);
+  };
 
   return (
     <>
       <Container>
         <section className={styles.login_page}>
-          <div className={styles.login_box}>
+          <form className={styles.login_box} onSubmit={handleSubmit(onSubmit)}>
             <FaRecycle className={styles.icon} />
             <label htmlFor="username">Usuário</label>
             <input
-              id="username"
+              {...register("username")}
               className={styles.text_input}
               type="text"
               placeholder="joaosilva"
               autoComplete="off"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
             />
             <label htmlFor="password">Senha</label>
             <input
+              {...register("funcionarioSenha")}
               id="password"
               className={styles.text_input}
               type="password"
               placeholder="digite sua senha"
               autoComplete="off"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
             />
-            <LoginHandler username={username} password={password} />
-          </div>
+            <button className={styles.login_button}>Login</button>
+          </form>
         </section>
       </Container>
       <Footer />
--- src/pages/Login/loginService.js
+++ src/pages/Login/loginService.js
@@ -1,15 +1,17 @@
-export function loginFetch(username, password) {
-    const apiPath = "/auth/login"
-    return fetch(import.meta.env.VITE_BACKEND_KEY + apiPath, {
-        method: "POST",
-        body: JSON.stringify({
-            username: username,
-            funcionarioSenha: password
-        }),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        }
-    })
-    .then(response => response.json());
-}
+import axios from "axios";
+const apiPath = "/auth/login";
 
+export function loginFetch(data, navigate) {
+  axios
+    .post(import.meta.env.VITE_BACKEND_KEY + apiPath, {
+      username: data.username,
+      funcionarioSenha: data.funcionarioSenha,
+    })
+    .then((response) => {
+      navigate("/home");
+    })
+
+    .catch((error) => {
+      console.log(error.message);
+    });
+}
